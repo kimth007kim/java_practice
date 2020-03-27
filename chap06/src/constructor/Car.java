e1eebf2c96ce09622ad3c9284991a6d6115d971c
kimth007kim <kimth007kim@naver.com>
2020-03-27 16:03:21 +0900

    클래스 생성자 오버로딩까지

---
+++ chap06/src/constructor/Car.java
@@ -0,0 +1,8 @@
+package constructor;
+
+public class Car {
+//»ı¼ºÀÚ
+	Car(String color, int cc){
+		
+	}
+}
---
+++ chap06/src/constructor_overloading/CarExample.java
@@ -0,0 +1,10 @@
+package constructor_overloading;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
