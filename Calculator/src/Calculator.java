7f06488409b0fa8a0a273cc43fa81bb6140da012
kimth007kim <kimth007kim@naver.com>
2020-03-31 17:38:29 +0900

    계산기 작업

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,33 +1,46 @@
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator extends JFrame{
+public class Calculator extends JFrame {
 
 	public Calculator() {
 		setTitle("계산기");
 		Container p = getContentPane();
-		//getContentPane:최근 버전에서는 사용하지 않아도 어느정도 보정이 되지만 안정성을 위해서 사용을 권한다.
+		// getContentPane:최근 버전에서는 사용하지 않아도 어느정도 보정이 되지만 안정성을 위해서 사용을 권한다.
 		p.setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		String result="";
 
 		JPanel jp = new JPanel();
-		jp.setLayout(new GridLayout(4,0,10,10));
-		
-		JTextField jf= new JTextField("계산식을 입력해주세요", 30);
+		jp.setLayout(new GridLayout(4, 0, 10, 10));
+
+		JTextField jf = new JTextField(result, 30);
 		p.add(jf);
 		p.add(jp);
-		
-		
+
 		JButton[] buttons = new JButton[10];
-		for(int i=0; i<10; i++) {
-			buttons[i]= new JButton(""+i);
+		for (int i = 0; i < 10; i++) {
+			buttons[i] = new JButton("" + i);
+			buttons[i].addMouseListener(new MouseAdapter() {
+
+				@Override
+				public void mousePressed(MouseEvent e) {
+					JButton b = (JButton)e.getSource();
+					result+=result+b.getText();
+					jf.setText(result);
+				}
+				
+			});
 			jp.add(buttons[i]);
 		}
 		JButton s1 = new JButton("+");
@@ -43,10 +56,9 @@ public class Calculator extends JFrame{
 		JButton EN = new JButton("↵ ");
 		jp.add(EN);
 		
-		
-		
-		
-		setSize(500,800);
+
+
+		setSize(500, 800);
 		setVisible(true);
 	}
 
