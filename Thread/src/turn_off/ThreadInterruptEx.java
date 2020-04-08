4698987f22843e6dd0b87313c477e409dfd63e3f
kimth007kim <kimth007kim@naver.com>
2020-04-08 20:59:34 +0900

    스레드죽이기 도전중

--- Thread/src/turn_off/ThreadInterruptEx.java
+++ Thread/src/turn_off/ThreadInterruptEx.java
@@ -1,15 +1,32 @@
 package turn_off;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+class TimerRunnable implements Runnable {
+	JLabel jla;
+	public TimerRunnable() {
+		this.jla=jla;
+	}
+	@Override
+	public void run() {
+		int n=0;
+		while(true) {
+			jla.setText(Integer.toString(n));
+		}
+	}
+
+}
 
 public class ThreadInterruptEx extends JFrame {
 	public ThreadInterruptEx() {
 		setTitle("Interrupted");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,500);
+		setSize(500, 500);
 		setVisible(true);
-		
+
 	}
+
 	public static void main(String[] args) {
 		new ThreadInterruptEx();
 	}
