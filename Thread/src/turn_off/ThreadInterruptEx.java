53682ac12b249ea462208e7a8ba5d5d6d1e28cf8
kimth007kim <kimth007kim@naver.com>
2020-04-09 23:32:53 +0900

    자바스크립트 과제

--- Thread/src/turn_off/ThreadInterruptEx.java
+++ Thread/src/turn_off/ThreadInterruptEx.java
@@ -1,11 +1,14 @@
 package turn_off;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 class TimerRunnable implements Runnable {
 	JLabel jla;
-	public TimerRunnable() {
+	public TimerRunnable(JLabel jla) {
 		this.jla=jla;
 	}
 	@Override
@@ -13,15 +16,29 @@ class TimerRunnable implements Runnable {
 		int n=0;
 		while(true) {
 			jla.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				return;
+			}
 		}
 	}
 
 }
 
 public class ThreadInterruptEx extends JFrame {
+	private Thread th;
 	public ThreadInterruptEx() {
 		setTitle("Interrupted");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container p =getContentPane();
+		p.setLayout(new FlowLayout());
+		
+		JLabel jla = new JLabel();
+		TimerRunnable runnable = new TimerRunnable(jla);
+		th = new Thread(runnable);
+		p.add(jla);
 		setSize(500, 500);
 		setVisible(true);
 
